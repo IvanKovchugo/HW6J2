f1112663fbb2bd2a3bbe3f3cf05162c1beaf3bad
kovsi <kovsiun@gmail.com>
2022-01-13 22:15:45 +0100

    S

---
+++ src/GeekBrains/Client.java
@@ -0,0 +1,120 @@
+package GeekBrains;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+        JTextArea in;
+        JTextField out;
+        BufferedReader reader;
+        PrintWriter writer;
+        Socket sock;
+
+    public class Reader implements Runnable {
+    public void run() {
+        String massage;
+        try {
+            while ((massage = reader.readLine()) !=null) {
+                System.out.println("Me " + massage);
+                in.append(massage + "\n");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+        private void setUpNetworking() {
+            try {
+                sock = new Socket("127.0.0.1", 5000);
+                InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(streamReader);
+                writer = new PrintWriter(sock.getOutputStream());
+                System.out.println("Соеденение установлено!");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    public void go() {
+        JFrame frame = new JFrame("Chat");
+        JPanel mainPanel = new JPanel();
+        in = new JTextArea(15, 50);
+        in.setLineWrap(true);
+        in.setWrapStyleWord(true);
+        in.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(in);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        out = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        sendButton.addKeyListener(new SendButtonListener());
+        mainPanel.add(scrollPane);
+        mainPanel.add(out);
+        mainPanel.add(sendButton);
+        setUpNetworking();
+
+        Thread readerTread = new Thread(new Reader());
+        readerTread.start();
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.go();
+    }
+
+    public class SendButtonListener implements ActionListener, KeyListener {
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+        @Override
+        public void  keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                try {
+                    writer.println((out.getText()));
+                    writer.flush();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                out.setText("");
+                out.requestFocus();
+                in.append(String.valueOf(writer));
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+
+        public void actionPerformed(ActionEvent e) {
+                try {
+                    writer.println(out.getText());
+                    writer.flush();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                out.setText("");
+                out.requestFocus();
+            }
+
+    }
+}
